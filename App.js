22ecf86d59c10c809793c2c9be2e4f2183058a7c
filipbura <fb52310@fer.hr>
2023-10-23 20:52:00 +0200

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textStyle}>Hello World</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textStyle: {
+    color: 'blue',
+    fontWeight: 'bold',
+    fontSize: 30,
+    textAlign: 'center',
+  },
+});
