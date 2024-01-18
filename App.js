ebef05aba4a2a36e3731c78a9fde3b35db6c2858
filipbura <fb52310@fer.hr>
2024-01-18 03:04:02 +0100

    long awaited quiz game page

--- App.js
+++ App.js
@@ -1,8 +1,4 @@
 import React from 'react';
-import {ScrollView, Text, TouchableOpacity, View} from 'react-native';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Footer from './components/Footer';
-import Header from './components/Header';
 import Home from './components/Home';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
@@ -10,7 +6,8 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Leagues from './components/Leagues';
 import League from './components/League';
 import Profile from './components/Profile';
-import SelectCategories from "./components/SelectCategories";
+import SelectCategories from './components/SelectCategories';
+import QuizGame from './components/QuizGame';
 
 const Stack = createNativeStackNavigator();
 const Tab = createBottomTabNavigator();
@@ -18,31 +15,38 @@ const Tab = createBottomTabNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Home' screenOptions={{animationEnabled: false}}>
+      <Stack.Navigator
+        initialRouteName="Home"
+        screenOptions={{animationEnabled: false}}>
         <Stack.Screen
-          name='Home'
+          name="Home"
           component={Home}
           options={{headerShown: false, animation: 'none'}}
         />
         <Stack.Screen
-          name='Leagues'
+          name="Leagues"
           component={Leagues}
           options={{headerShown: false, animation: 'none'}}
         />
         <Stack.Screen
-          name='League'
+          name="League"
           component={League}
           options={{headerShown: false, animation: 'none'}}
         />
         <Stack.Screen
-          name='Profile'
+          name="Profile"
           component={Profile}
           options={{headerShown: false, animation: 'none'}}
         />
         <Stack.Screen
-            name='SelectCategories'
-            component={SelectCategories}
-            options={{headerShown: false, animation: 'none'}}
+          name="SelectCategories"
+          component={SelectCategories}
+          options={{headerShown: false, animation: 'none'}}
+        />
+        <Stack.Screen
+          name="QuizGame"
+          component={QuizGame}
+          options={{headerShown: false, animation: 'none'}}
         />
       </Stack.Navigator>
     </NavigationContainer>
--- components/Home.js
+++ components/Home.js
@@ -1,12 +1,17 @@
-import {Text, View, StyleSheet, Pressable, TouchableOpacity, ScrollView} from 'react-native';
+import {
+  Pressable,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import * as Progress from 'react-native-progress';
 import IconM from 'react-native-vector-icons/MaterialCommunityIcons';
-import IconE from 'react-native-vector-icons/Entypo';
-import {getHomePageData} from "../stores/homeStore";
-import Footer from './Footer'
-import SelectCategories from "./SelectCategories";
+import {getHomePageData} from '../stores/homeStore';
+import Footer from './Footer';
+import SelectCategories from './SelectCategories';
 
 export default function Home({navigation}) {
   const [homeData, setHomeData] = useState({});
@@ -17,109 +22,113 @@ export default function Home({navigation}) {
     Sport: 'trophy',
     Art: 'palette',
     Geography: 'globe-americas',
-    Movies: 'film'
-  }
+    Movies: 'film',
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     getHomePageData(4).then(res => {
       setHomeData(res);
     });
-  },[]);
+  }, []);
 
   return (
-    <View className='flex-1'>
+    <View className="flex-1">
       <ScrollView>
-        <View className='flex-1 justify-start items-start m-5'>
-          <Text className='text-lg text-blue-800'>Welcome back, {homeData.username && homeData.username}</Text>
-          <Text className='font-black text-3xl text-blue-950'>Let's play!</Text>
-          <View className='bg-blue-950 rounded-2xl p-5 w-full mt-7'>
-            <View className='flex flex-row justify-between items-center'>
-              <View className='flex flex-col justify-center items-center'>
-                <Text className='text-white font-bold text-lg mb-3'>{homeData.daysInARow && homeData.daysInARow} days strike!</Text>
-                <View className='flex justify-start items-center flex-row'>
-                  <Icon
-                    name='gem'
-                    size={14}
-                    color='white'
-                  />
-                  <Text className='text-white ml-1'>+10 daily points</Text>
+        <View className="flex-1 justify-start items-start m-5">
+          <Text className="text-lg text-blue-800">
+            Welcome back, {homeData.username && homeData.username}
+          </Text>
+          <Text className="font-black text-3xl text-blue-950">Let's play!</Text>
+          <View className="bg-blue-950 rounded-2xl p-5 w-full mt-7">
+            <View className="flex flex-row justify-between items-center">
+              <View className="flex flex-col justify-center items-center">
+                <Text className="text-white font-bold text-lg mb-3">
+                  {homeData.daysInARow && homeData.daysInARow} days strike!
+                </Text>
+                <View className="flex justify-start items-center flex-row">
+                  <Icon name="gem" size={14} color="white" />
+                  <Text className="text-white ml-1">+10 daily points</Text>
                 </View>
               </View>
-              <View className='mr-4'>
-                <Icon
-                  name='gem'
-                  size={28}
-                  color='white'
-                />
+              <View className="mr-4">
+                <Icon name="gem" size={28} color="white" />
               </View>
             </View>
-            <Progress.Bar progress={homeData.daysInARow ? homeData.daysInARow / 20 : 0.0} width={null} className='bg-white mt-2' />
+            <Progress.Bar
+              progress={homeData.daysInARow ? homeData.daysInARow / 20 : 0.0}
+              width={null}
+              className="bg-white mt-2"
+            />
           </View>
-          <Text className='mt-6 text-blue-950 font-bold text-lg'>
-              Daily quiz
+          <Text className="mt-6 text-blue-950 font-bold text-lg">
+            Daily quiz
           </Text>
-          <View className='border-2 border-gray-300 rounded-2xl p-5 w-full mt-2'>
-              <View className='flex flex-row justify-between items-center'>
-                  <View className='flex flex-col items-center'>
-                      <Text className='text-blue-950 font-bold text-xl mb-1'>
-                          Compete globally
-                      </Text>
-                      <Text className='mb-4 text-blue-800'>+ 10k users</Text>
-                      {!homeData.globalDone ? (
-                          <Pressable className='bg-blue-400 py-2 px-3 rounded-md'>
-                            <Text className='text-white'>Play now!</Text>
-                          </Pressable>
-                      ) : (
-                          <View className='bg-gray-300 py-2 px-3 rounded-md'>
-                            <Text className='text-white'>Come back tommorow</Text>
-                          </View>
-                      )}
-
+          <View className="border-2 border-gray-300 rounded-2xl p-5 w-full mt-2">
+            <View className="flex flex-row justify-between items-center">
+              <View className="flex flex-col items-center">
+                <Text className="text-blue-950 font-bold text-xl mb-1">
+                  Compete globally
+                </Text>
+                <Text className="mb-4 text-blue-800">+ 10k users</Text>
+                {!homeData.globalDone ? (
+                  <Pressable className="bg-blue-400 py-2 px-3 rounded-md">
+                    <Text
+                      className="text-white"
+                      onPress={() =>
+                        navigation.navigate('QuizGame', {
+                          userId: 4,
+                          leagueId: 1,
+                        })
+                      }>
+                      Play now!
+                    </Text>
+                  </Pressable>
+                ) : (
+                  <View className="bg-gray-300 py-2 px-3 rounded-md">
+                    <Text className="text-white">Come back tommorow</Text>
                   </View>
-                  <View className='mr-5'>
-                      <IconM
-                      name='brain'
-                      size={44}
-                      color='#172554'
-                      />
-                  </View>
+                )}
               </View>
+              <View className="mr-5">
+                <IconM name="brain" size={44} color="#172554" />
+              </View>
+            </View>
           </View>
-          <Text className='mt-6 text-blue-950 font-bold text-lg mb-2'>
+          <Text className="mt-6 text-blue-950 font-bold text-lg mb-2">
             Favourite categories
           </Text>
-          <View className='flex flex-row justify-between w-full items-center flex-wrap'>
-            {homeData.favouriteCategories && homeData.favouriteCategories.map(cat => (
+          <View className="flex flex-row justify-between w-full items-center flex-wrap">
+            {homeData.favouriteCategories &&
+              homeData.favouriteCategories.map(cat => (
                 <TouchableOpacity
-                    className='flex flex-col justify-evenly items-center border-2 border-gray-300 rounded-xl w-40 h-32'
-                    key={cat.categoryName}>
+                  className="flex flex-col justify-evenly items-center border-2 border-gray-300 rounded-xl w-40 h-32"
+                  key={cat.categoryName}>
                   <Icon
-                      name={categoryIcons[cat.categoryName]}
-                      size={50}
-                      color='#172554'
+                    name={categoryIcons[cat.categoryName]}
+                    size={50}
+                    color="#172554"
                   />
-                  <Text className='text-blue-950 text-lg font-bold'>
+                  <Text className="text-blue-950 text-lg font-bold">
                     {cat.categoryName}
                   </Text>
                 </TouchableOpacity>
-
-            ))}
+              ))}
             <TouchableOpacity
-              className='flex flex-col justify-evenly items-center border-2 border-gray-300 rounded-xl w-40 h-32 mt-7'
-                onPress={() => navigation.navigate('SelectCategories', {selected: homeData.favouriteCategories.map(cat => cat.categoryName)})}>
-              <Icon
-                name='plus'
-                size={50}
-                color='#172554'
-              />
-              <Text className='text-blue-950 text-lg font-bold '>
-                Add new
-              </Text>
+              className="flex flex-col justify-evenly items-center border-2 border-gray-300 rounded-xl w-40 h-32 mt-7"
+              onPress={() =>
+                navigation.navigate('SelectCategories', {
+                  selected: homeData.favouriteCategories.map(
+                    cat => cat.categoryName,
+                  ),
+                })
+              }>
+              <Icon name="plus" size={50} color="#172554" />
+              <Text className="text-blue-950 text-lg font-bold ">Add new</Text>
             </TouchableOpacity>
           </View>
         </View>
       </ScrollView>
-      <Footer navigation={navigation} current="home"/>
+      <Footer navigation={navigation} current="home" />
     </View>
   );
 }
