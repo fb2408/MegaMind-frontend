6696f29aeb1d7bdb529cefe4f6d5a5b815507722
filipbura <fb52310@fer.hr>
2024-01-19 10:07:10 +0100

    fixed bug

--- components/SignUp.js
+++ components/SignUp.js
@@ -1,14 +1,22 @@
 import LinearGradient from 'react-native-linear-gradient';
-import {Alert, Modal, ScrollView, Text, TextInput, TouchableOpacity, View, StyleSheet, Pressable} from 'react-native';
+import {
+  Alert,
+  Modal,
+  Pressable,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome5';
 import {useEffect, useState} from 'react';
 import {useIsFocused} from '@react-navigation/native';
-import {getHomePageData} from '../stores/homeStore';
 import {getCategoriesForLeague} from '../stores/leagueStore';
 import {registerPost} from '../stores/loginStore';
 
 export default function SignUp({navigation}) {
-
   const [username, setUserame] = useState('');
   const [password, setPassword] = useState('');
   const [firstName, setFirstName] = useState('');
@@ -17,24 +25,24 @@ export default function SignUp({navigation}) {
   const [categories, setCategories] = useState([]);
   const [selectedCategories, setSelectedCategories] = useState([]);
 
-  const isFocused = useIsFocused()
+  const isFocused = useIsFocused();
 
   useEffect(() => {
-    if(isFocused){
+    if (isFocused) {
       getCategoriesForLeague().then(res => {
         setCategories(res.categories);
       });
     }
-  }, [isFocused])
+  }, [isFocused]);
 
-  const handlePress = (categoryId) => {
-      if(selectedCategories.includes(categoryId)) {
-        const newCategories = selectedCategories.filter((id) => id !== categoryId);
-        setSelectedCategories(newCategories)
-      } else {
-        setSelectedCategories((prevCategories) => [...prevCategories, categoryId]);
-      }
-  }
+  const handlePress = categoryId => {
+    if (selectedCategories.includes(categoryId)) {
+      const newCategories = selectedCategories.filter(id => id !== categoryId);
+      setSelectedCategories(newCategories);
+    } else {
+      setSelectedCategories(prevCategories => [...prevCategories, categoryId]);
+    }
+  };
 
   const submitRegistration = () => {
     const data = {
@@ -42,72 +50,82 @@ export default function SignUp({navigation}) {
       firstname: firstName,
       lastname: lastName,
       password: password,
-      favouriteCategoryIds: selectedCategories
-    }
+      favouriteCategoryIds: selectedCategories,
+    };
 
     registerPost(data).then(res => {
-      console.log(res)
-      navigation.navigate('Home', {userId: res.id})
-    })
-  }
+      console.log(res);
+      navigation.navigate('Home', {userId: res.id, username: username});
+    });
+  };
 
   return (
-    <View className='flex-1'>
+    <View className="flex-1">
       <ScrollView>
         <LinearGradient
           colors={['rgb(30 64 175)', 'rgb(23 37 84)']}
-          useAngle={true} angle={45} angleCenter={{x: 0.2, y: 0.5}}
-          className='flex-1 items-center justify-start w-full px-10 py-5'>
-          <View className='flex justify-center items-center mb-5'>
-            <Text className='text-white text-4xl mt-6' style={{fontFamily: 'ShantellSans-Bold'}}>MegaMind</Text>
-            <Icon
-              color='white'
-              name='lightbulb'
-              size={60}
-            />
+          useAngle={true}
+          angle={45}
+          angleCenter={{x: 0.2, y: 0.5}}
+          className="flex-1 items-center justify-start w-full px-10 py-5">
+          <View className="flex justify-center items-center mb-5">
+            <Text
+              className="text-white text-4xl mt-6"
+              style={{fontFamily: 'ShantellSans-Bold'}}>
+              MegaMind
+            </Text>
+            <Icon color="white" name="lightbulb" size={60} />
           </View>
-          <View className='flex justify-start items-center rounded-xl bg-white w-full p-5'>
-            <Text className='text-blue-950 text-4xl my-2' style={{fontFamily: 'ShantellSans-Bold'}}>Registration</Text>
-            <View className='flex justify-start items-start w-full p-2'>
-              <Text className='text-blue-950 text-lg'>Username</Text>
+          <View className="flex justify-start items-center rounded-xl bg-white w-full p-5">
+            <Text
+              className="text-blue-950 text-4xl my-2"
+              style={{fontFamily: 'ShantellSans-Bold'}}>
+              Registration
+            </Text>
+            <View className="flex justify-start items-start w-full p-2">
+              <Text className="text-blue-950 text-lg">Username</Text>
               <TextInput
                 onChangeText={setUserame}
-                className='rounded-md h-14 p-4 w-full shadow-sm shadow-blue-950'
-                placeholder='example123'
+                className="rounded-md h-14 p-4 w-full shadow-sm shadow-blue-950"
+                placeholder="example123"
               />
             </View>
-            <View className='flex justify-start items-start w-full p-2'>
-              <Text className='text-blue-950 text-lg'>First name</Text>
+            <View className="flex justify-start items-start w-full p-2">
+              <Text className="text-blue-950 text-lg">First name</Text>
               <TextInput
                 onChangeText={setFirstName}
-                className='rounded-md h-14 p-4 w-full shadow-sm shadow-blue-950'
-                placeholder='Ivo'
+                className="rounded-md h-14 p-4 w-full shadow-sm shadow-blue-950"
+                placeholder="Ivo"
               />
             </View>
-            <View className='flex justify-start items-start w-full p-2'>
-              <Text className='text-blue-950 text-lg'>Last name</Text>
+            <View className="flex justify-start items-start w-full p-2">
+              <Text className="text-blue-950 text-lg">Last name</Text>
               <TextInput
                 onChangeText={setLastName}
-                className='rounded-md h-14 p-4 w-full shadow-sm shadow-blue-950'
-                placeholder='Ivić'
+                className="rounded-md h-14 p-4 w-full shadow-sm shadow-blue-950"
+                placeholder="Ivić"
               />
             </View>
-            <View className='flex justify-start items-start w-full p-2'>
-              <Text className='text-blue-950 text-lg'>Password</Text>
+            <View className="flex justify-start items-start w-full p-2">
+              <Text className="text-blue-950 text-lg">Password</Text>
               <TextInput
                 onChangeText={setPassword}
-                className='rounded-md h-14 p-4 w-full shadow-sm shadow-blue-950'
-                placeholder='****'
+                className="rounded-md h-14 p-4 w-full shadow-sm shadow-blue-950"
+                placeholder="****"
                 secureTextEntry={true}
               />
             </View>
-            <View className='flex justify-start items-start w-full p-2'>
-              <Text className='text-blue-950 text-lg'>Favourite categories</Text>
+            <View className="flex justify-start items-start w-full p-2">
+              <Text className="text-blue-950 text-lg">
+                Favourite categories
+              </Text>
               <TouchableOpacity
                 onPress={() => setModalVisible(true)}
                 className="w-full flex justify-center items-center p-2 rounded-md mt-2"
-              style={{backgroundColor: "#63E6BE"}}>
-                <Text className="text-white text-lg font-semibold">Choose...</Text>
+                style={{backgroundColor: '#63E6BE'}}>
+                <Text className="text-white text-lg font-semibold">
+                  Choose...
+                </Text>
               </TouchableOpacity>
             </View>
 
@@ -121,17 +139,26 @@ export default function SignUp({navigation}) {
               }}>
               <View style={styles.centeredView}>
                 <View style={styles.modalView}>
-                  {categories && categories.map((category, index) => {
-                    return(
-                      <Pressable
-                        style={{backgroundColor: selectedCategories.includes(category.id) ? "#63E6BE" : "rgb(30 58 138)"}}
-                        key={index}
-                        onPress={() => handlePress(category.id)}
-                        className="w-48 h-12 rounded-xl mb-2 flex justify-center items-center ">
-                        <Text className="text-white font-semibold">{category.name}</Text>
-                      </Pressable>
-                    )
-                  })}
+                  {categories &&
+                    categories.map((category, index) => {
+                      return (
+                        <Pressable
+                          style={{
+                            backgroundColor: selectedCategories.includes(
+                              category.id,
+                            )
+                              ? '#63E6BE'
+                              : 'rgb(30 58 138)',
+                          }}
+                          key={index}
+                          onPress={() => handlePress(category.id)}
+                          className="w-48 h-12 rounded-xl mb-2 flex justify-center items-center ">
+                          <Text className="text-white font-semibold">
+                            {category.name}
+                          </Text>
+                        </Pressable>
+                      );
+                    })}
                   <Pressable
                     style={[styles.button, styles.buttonClose]}
                     onPress={() => setModalVisible(!modalVisible)}>
@@ -141,11 +168,13 @@ export default function SignUp({navigation}) {
               </View>
             </Modal>
 
-            <View className='flex flex-row justify-center items-center'>
+            <View className="flex flex-row justify-center items-center">
               <TouchableOpacity
                 onPress={() => submitRegistration()}
-                className='flex justify-center items-center bg-blue-500 mt-4 px-5 py-3 rounded-md'>
-                <Text className='text-white text-base font-semibold'>Sign up</Text>
+                className="flex justify-center items-center bg-blue-500 mt-4 px-5 py-3 rounded-md">
+                <Text className="text-white text-base font-semibold">
+                  Sign up
+                </Text>
               </TouchableOpacity>
             </View>
           </View>
